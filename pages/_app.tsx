3fcbe20c845d0385956f2b53afecd570a0bc0ab3
Karel Durdis <karel.durdis@gmail.com>
2022-05-06 13:12:33 +0200

    Add React Query and axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </NextUIProvider>
   );
 }
