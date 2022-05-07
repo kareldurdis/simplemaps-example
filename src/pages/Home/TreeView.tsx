da2bfa3e68b36c76d53933ff4ee8f8163a6ffc94
Karel Durdis <karel.durdis@gmail.com>
2022-05-07 21:13:27 +0200

    Update TreeView to set clicked element to parent state

--- src/pages/Home/TreeView.tsx
+++ src/pages/Home/TreeView.tsx
@@ -1,14 +1,19 @@
 import React, { CSSProperties, memo } from 'react';
 import { FixedSizeList as List, areEqual } from 'react-window';
-import { Col, Text } from '@nextui-org/react';
-import type { State } from './index';
+import { Card, Col, Divider, Text } from '@nextui-org/react';
+import { State } from 'src/utils/createTree';
 import styles from './TreeView.module.css';
 
 type TreeViewProps = {
   treeData: State[];
+  setActiveItem: any;
 };
 
 type RowProps = {
+  data: {
+    items: State[];
+    toggleItemActive: () => void;
+  };
   index: number;
   style: CSSProperties;
 };
@@ -19,34 +24,43 @@ const Row = memo(({ data, index, style }) => {
   const item = items[index];
 
   return (
-    <div onClick={() => toggleItemActive(index)} style={style}>
-      {item.label} is {item.isActive ? 'active' : 'inactive'}
+    <div onClick={() => toggleItemActive(item)} style={style}>
+      {item.name}
     </div>
   );
 }, areEqual);
+Row.displayName = 'Row';
 
-const TreeView = ({ treeData }: TreeViewProps) => {
-  console.log(treeData);
+const createItemData = (items: State[], toggleItemActive: any) => ({
+  items,
+  toggleItemActive,
+});
 
-  const Row = React.memo(({ index, style }: RowProps) => (
-    <div className={index % 2 ? styles.ListItemOdd : styles.ListItemEven} style={style}>
-      {treeData[index].name}
-    </div>
-  ));
-  Row.displayName = 'Row';
+const TreeView = ({ treeData, setActiveItem }: TreeViewProps) => {
+  // console.log(treeData);
+
+  const itemData = createItemData(treeData, setActiveItem);
 
   return (
     <Col>
-      <Text h2>State</Text>
-      <List
-        className={styles.List}
-        height={300}
-        itemCount={treeData.length}
-        itemSize={35}
-        width={300}
-      >
-        {Row}
-      </List>
+      <Card css={{ mw: '330px' }}>
+        <Card.Header>
+          <Text h3>State</Text>
+        </Card.Header>
+        <Divider />
+        <Card.Body css={{ py: '$10' }}>
+          <List
+            className={styles.List}
+            height={300}
+            itemCount={treeData.length}
+            itemSize={35}
+            itemData={itemData}
+            width={290}
+          >
+            {Row}
+          </List>
+        </Card.Body>
+      </Card>
     </Col>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,11 @@ import Head from 'next/head';
 import { Container, Row, Text, Spacer } from '@nextui-org/react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { useState } from 'react';
 import { Database } from 'src/../scripts/import';
 import TreeView from 'src/pages/Home/TreeView';
 import createTree, { State } from 'src/utils/createTree';
+import StatusOrChildren from 'src/components/StatusOrChildren';
 
 const useCities = () => {
   return useQuery<Database, Error>('cities', async () => {
@@ -16,12 +18,15 @@ const useCities = () => {
 
 const Home: NextPage = () => {
   const { status, data, error } = useCities();
+  const [activeItem, setActiveItem] = useState();
 
   let treeData: State[] = [];
 
   if (status === 'success') {
     treeData = createTree(data);
   }
+
+  console.log('activeItem', activeItem);
 
   return (
     <>
@@ -34,11 +39,14 @@ const Home: NextPage = () => {
           <Row justify="center" align="center">
             <Text h1>US States, Counties and Cities</Text>
           </Row>
+          <Row justify="center" align="center">
+            <Text>Pick a State, County or City to see details</Text>
+          </Row>
           <Spacer />
           <Row justify="center" align="center">
-            {status === 'loading' && <Loading size="xl" />}
-            {status === 'error' && <Card color="error">{error.message}</Card>}
-            {status === 'success' && <TreeView treeData={treeData} />}
+            <StatusOrChildren status={status} error={error}>
+              <TreeView treeData={treeData} setActiveItem={setActiveItem} />
+            </StatusOrChildren>
           </Row>
         </Container>
       </main>
